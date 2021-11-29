51a8de8a13a70b5228b7100a7651b606fd8ef2d5
Antoni Pstraś <antek.pstras@gmail.com>
2021-11-29 10:59:58 +0100

    Changed dir of vcr records

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -70,7 +70,7 @@ RSpec.configure do |config|
 end
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :faraday
 
   config.configure_rspec_metadata!
